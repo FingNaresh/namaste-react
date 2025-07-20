df1e001110ebc8ffdad2365fd4a51d295a60f360
FingNaresh <nareshyadavansi3565@gmail.com>
2025-07-20 22:02:22 +0530

    hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,29 @@
 import RestaurantCard from './RestaurantCard';
+import { useState } from 'react';
 import resList from '../utils/mockData';
 
 
 const Body = () => {
+  //local state variable - super powerful variable
+  const[listofRestaurants, setListOfRestaurants] = useState(resList);
+
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+    <div className="filter">
+      <button 
+        className='filter-btn'
+         onClick={()=>{
+          const filteredList = listofRestaurants.filter(
+            (res)=>res.data.avgRating>4
+            );
+            setListOfRestaurants(filteredList);
+         }}
+         >
+        Top Rated Restaurants</button>
+    </div>
       <div className='res-container'>
-        {resList.map((restaurant) =>(
+        {listofRestaurants.map((restaurant) =>(
           <RestaurantCard key={restaurant.data.id} resData={restaurant}/>
         ))}
       </div>
