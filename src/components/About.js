6ed69d309a2001f4cdc7065e6341ee5d1f59b876
FingNaresh <nareshyadavansi3565@gmail.com>
2025-07-22 15:12:14 +0530

    component

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,35 @@
-import React from 'react'
+import {Component} from 'react'
+import User from './User'
+import UserClass from './UserClass'
 
-const About = () => {
-  return (
-    <div>
-        <h1>About Us</h1>
+class About extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+   componentDidMount() {
+        // console.log("Parent component mounted");
+   }
+  
+
+  
+
+  render() {
+    return (
+      <div>
+        <h1>About  Class Component</h1>
         <h2>We are a team of passionate developers.</h2>
+       
+        <UserClass name={"First"} location={"Pune class"} />
         <p>Our mission is to create amazing web applications that enhance user experience.</p>
-    </div>
-  )
+      </div>
+    );
+  }
 }
+
+
+
+
 
 export default About
---
+++ src/components/UserClass.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+class UserClass extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state={
+            userInfo:{
+            name:"Dummy",
+            loaction:"Default",
+        },
+    };
+
+     }
+    
+
+    async componentDidMount() {
+        // console.log("UserClass component mounted");
+        // Api call
+        const data = await fetch("https://api.github.com/users/FingNaresh");
+        const json =await  data.json();
+
+        this.setState({
+            userInfo:json,
+        });
+        console.log(json);
+        
+    }
+
+    componentDidUpdate(){
+        console.log("Component Did update")
+    }
+
+    componentWillUnmount(){
+        console.log("Component will Unmount")
+    }
+
+    
+
+  
+  render() {
+    const {name, location, avatar_url} = this.state.userInfo;
+
+    return (
+      // Your JSX goes here
+      <div className='user-card'>
+        <img src={avatar_url}/>
+      <h2>Name: {name}</h2>
+      <h3>Location: {location}</h3>
+      <h4>Contact:pinncacleboy@gmail.com</h4>
+      </div>
+    );
+  }
+}
+
+export default UserClass;
