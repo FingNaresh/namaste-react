e6264192ff573fbae967478cce60596b941c86de
FingNaresh <nareshyadavansi3565@gmail.com>
2025-07-22 10:21:46 +0530

    menu

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,5 +4,7 @@ export const CDN_URL=
 export const LOGO_URL =
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLZoWfF8Q6y-Oeh4eoAuWx1gqcdpQ19V5JV12JtR41cHR48SZA"
 
-// 
+export const MENU_API =
+        "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5246091&lng=73.8786239&restaurantId="
+
         
